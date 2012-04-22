917e868bd778d1713e71bbc325ae9779e6dda4b3
angelinavj <angelina.veni@gmail.com>
2012-04-22 12:36:15 -0700

    Skeleton code for PA2

---
+++ tree.d
@@ -0,0 +1,6 @@
+tree.o tree.d : tree.cc /afs/ir/class/cs143/cool/sp12/include/PA3/copyright.h \
+ /afs/ir/class/cs143/cool/sp12/include/PA3/tree.h \
+ /afs/ir/class/cs143/cool/sp12/include/PA3/copyright.h \
+ /afs/ir/class/cs143/cool/sp12/include/PA3/stringtab.h \
+ /afs/ir/class/cs143/cool/sp12/include/PA3/list.h \
+ /afs/ir/class/cs143/cool/sp12/include/PA3/cool-io.h
---
+++ utilities.d
@@ -0,0 +1,11 @@
+utilities.o utilities.d : utilities.cc \
+ /afs/ir/class/cs143/cool/sp12/include/PA3/copyright.h \
+ /afs/ir/class/cs143/cool/sp12/include/PA3/cool-io.h \
+ /afs/ir/class/cs143/cool/sp12/include/PA3/copyright.h \
+ /afs/ir/class/cs143/cool/sp12/include/PA3/cool-parse.h \
+ /afs/ir/class/cs143/cool/sp12/include/PA3/cool-io.h \
+ /afs/ir/class/cs143/cool/sp12/include/PA3/tree.h \
+ /afs/ir/class/cs143/cool/sp12/include/PA3/stringtab.h \
+ /afs/ir/class/cs143/cool/sp12/include/PA3/list.h \
+ /afs/ir/class/cs143/cool/sp12/include/PA3/stringtab.h \
+ /afs/ir/class/cs143/cool/sp12/include/PA3/utilities.h
